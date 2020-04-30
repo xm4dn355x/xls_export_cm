881643e0dc70f30c6a1d812b0c532d4948f9e897
Michael Nikitenko <xm4dn355x@gmail.com>
2020-04-30 19:45:53 +0500

    added docstrings

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ from psycopg2.extras import DictCursor
 
 
 def get_data_from_db():
+    """
+    Return list of users in dict format
+
+    :return: list of dicts
+    """
     conn = psycopg2.connect(dbname=DB_CONFIG['dbname'],
                             user=DB_CONFIG['user'],
                             password=DB_CONFIG['password'],
@@ -26,30 +31,39 @@ def get_data_from_db():
 
 
 def create_xls(data):
-    print(data)
+    """
+    Recieve list of dicts with DB data and return create "Авторизация СЦ.xls" file.
+
+    :param data: list of dicts with DB data
+    """
+    # Creating book and sheet
     book = xlwt.Workbook('utf8')
     sheet = book.add_sheet('Зарегистрированные пользователи')
 
+    # Table header style
     header_style = xlwt.XFStyle()
     header_font = xlwt.Font()
     header_font.name = 'Arial'
     header_font.bold = True
     header_font.colour_index = xlwt.Style.colour_map['black']
-    header_font.height = 240
+    header_font.height = 260
     header_style.font = header_font
 
+    # Data rows styles. Inherits Header style.
     data_style = xlwt.XFStyle()
     data_font = xlwt.Font()
-    data_font.name = 'Arial'
+    data_font.name = header_font.name
+    data_font.colour_index = header_font.colour_index
     data_font.bold = False
-    data_font.colour_index = xlwt.Style.colour_map['black']
     data_font.height = 240
     data_style.font = data_font
 
+    # TamTam id col data style. Inherits Data style
     tt_id_style = xlwt.XFStyle()
     tt_id_style.font = data_font
     tt_id_style.num_format_str = '0'
 
+    # Creating borders
     borders = xlwt.Borders()
     borders.left = 1
     borders.right = 1
@@ -59,6 +73,7 @@ def create_xls(data):
     data_style.borders = borders
     tt_id_style.borders = borders
 
+    # Creating text alignment
     al = xlwt.Alignment()
     al.horz = al.HORZ_CENTER
     al.vert = al.VERT_CENTER
@@ -68,8 +83,7 @@ def create_xls(data):
     data_style.alignment = al
     tt_id_style.alignment = al
 
-
-    # Объявляем заголовок
+    # Table Header declaration
     sheet.write(0, 0, '№', header_style)
     sheet.write(0, 1, 'ТамТам id', header_style)
     sheet.write(0, 2, 'ТамТам имя пользователя', header_style)
@@ -86,7 +100,7 @@ def create_xls(data):
     sheet.col(5).width = 24500
     sheet.col(6).width = 3275
 
-    # Добавляем данные из базы
+    # Adding data rows
     i = 1
     for d in data:
         sheet.row(i+1).height = 2500
@@ -99,7 +113,7 @@ def create_xls(data):
         sheet.write(i, 6, d['chats'], data_style)
         i = i + 1
 
-    # Сохраняем документ
+    # Save XLS document
     sheet.portrait = False
     sheet.set_print_scaling(100)
     book.save('Авторизация СЦ.xls')
